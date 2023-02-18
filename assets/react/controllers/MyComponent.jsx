e1ea8729bb4ccc7c6f168f93a5673bf0742871dc
Pitsis <kontospan@gmail.com>
2023-02-18 19:38:28 +0200

    add react to symfony's front end

---
+++ assets/react/controllers/MyComponent.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+
+export default function (props) {
+  return (
+    <Paper elevation={3}>
+      <Button variant="text">Text</Button>
+    </Paper>
+  );
+}
